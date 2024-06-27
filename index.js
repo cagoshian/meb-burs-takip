7b0883b9c376fb9037b85b568b98f31b51a4e066
cagoshian <cagankota@gmail.com>
2024-06-27 17:10:39 +0300

    upload first version

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const axios = require('axios');
+const jssoup = require('jssoup').default;
+
+const pushbullet_api_key = "" //telefona bildirim göndermek için Pushbullet hesabının API anahtarı
+const w3a = "" //e-devlet w3a çerezi
+
+const url = 'https://www.turkiye.gov.tr/ptt-ptt-uzerinden-yapilan-kurum-odemeleri-sorgulama'
